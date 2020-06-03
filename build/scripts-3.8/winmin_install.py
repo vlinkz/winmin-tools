b44cc6bfcd7625db4d5b5004ac26bc0e1a5c1572
Victor Fuentes <hyruleterminatriforce@gmail.com>
2020-06-03 18:15:29 -0400

    Add license

--- build/scripts-3.8/winmin_install.py
+++ build/scripts-3.8/winmin_install.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import os,sys,subprocess
 import argparse
 import time
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from setuptools import setup, find_packages
 from os import path
 
